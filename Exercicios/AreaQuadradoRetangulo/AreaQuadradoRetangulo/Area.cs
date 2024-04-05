f0120660ed55990ca42523333aa333fc60cc77fe
paulo aug <124801497+pauloaug07@users.noreply.github.com>
2024-04-05 12:46:50 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+        public int AreaCalc { get; set; }
+        
+        public int Calc()
+        {
+            AreaCalc = Base * Altura;
+            return AreaCalc;
+        }
+
+        public string Calc2()
+        {
+            string resultado = "";
+            AreaCalc = Base * Altura;
+
+            if (Base == Altura)
+            {
+                resultado = "quadrado: " + AreaCalc;
+            }
+            else
+            {
+                resultado = "retângulo: " + AreaCalc;
+            }
+            
+            return resultado;
+        }
+
+        public void ExibeValor()
+        {
+            Console.WriteLine("Base: " + Base);
+            Console.WriteLine("Altura: " + Altura);
+            Console.WriteLine("Área: " + AreaCalc);
+        }
+    }
+}
---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de area de quadrado/retangulo");
+           
+            Area area1= new Area();
+
+            Console.WriteLine("Digite o valor da base:");
+            area1.Base = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura:");
+            area1.Altura = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Valor da área: " + area1.Calc());
+            Console.WriteLine("A área do " + area1.Calc2());
+
+            area1.ExibeValor();
+        }
+    }
+}
