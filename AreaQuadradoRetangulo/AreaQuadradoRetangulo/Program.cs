1f8812076b358f733a817b87189723c8a988ee0f
paulo aug <124801497+pauloaug07@users.noreply.github.com>
2024-04-11 23:02:30 -0300

    Add files via upload

---
+++ AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de area de quadrado/retangulo");
+
+            Area figura1 = new Area();
+            figura1.Calculo();
+            Console.WriteLine(figura1.MostrarDados());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExercicioFotos/ExercicioFotos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioFotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de caso certo:");
+            AlbumDeFotos albumCerto = new AlbumDeFotos();
+            albumCerto.CriarAlbum(42, 20);
+            
+            Console.WriteLine("======================");
+            Console.WriteLine("======================");
+            Console.WriteLine("======================");
+           
+            Console.WriteLine("Exemplo de caso errado:");
+            AlbumDeFotos albumErrado = new AlbumDeFotos();
+            albumErrado.CriarAlbum(40, 84);
+        }
+    }
+}
