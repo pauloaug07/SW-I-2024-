373fa64d49aab56448892c941af64a1ec2366551
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-06-14 12:39:16 -0300

    'mvc'

---
+++ ProjetosASP.NET/CadastroClientes/CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+    }
+}
